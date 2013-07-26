b5ced8317be1ddd7bce29a72317066f3398ab0d2
Tadas Tamosauskas <tadas@pdfcv.com>
2013-07-26 14:07:45 +0100

    NER support

--- index.py
+++ index.py
@@ -3,11 +3,7 @@ import sys
 import urllib2
 
 SOLR_URL = "http://localhost:8983/solr/update/json"
-
-
-with open("config/schema_mapper.json") as f:
-    map = json.loads(f.read())
-
+NER_SERVER = "http://192.168.1.31:8080/"
 
 def tweets():
     with open(sys.argv[1]) as f:
@@ -31,14 +27,31 @@ def value_for_path(path, tweet):
     return value
 
 
-for tweet in tweets():
-    doc = {}
-    for path, field in map.iteritems():
-        value = value_for_path(path, tweet)
-        doc[field] = value
-
+def index(doc):
     solrReq = urllib2.Request(SOLR_URL, json.dumps([doc]))
     solrReq.add_header("Content-Type", "application/json")
     solrPoster = urllib2.urlopen(solrReq)
     response = solrPoster.read()
     solrPoster.close()
+
+
+def extract(text):
+    response = urllib2.urlopen(NER_SERVER, text.encode("UTF-8", "ignore"))
+    return json.loads(response.read())
+
+
+with open("config/schema_mapper.json") as f:
+    map = json.loads(f.read())
+
+count = 0
+for tweet in tweets():
+    doc = {}
+    count += 1
+    for path, field in map.iteritems():
+        value = value_for_path(path, tweet)
+        doc[field] = value
+    for k, v in extract(doc['text']).iteritems():
+        doc["ner_%s" % k.lower()] = v
+    if count % 1000 == 0:
+        print count
+    index(doc)
